2255c3579fecb429c6be07c047bd977d44d508b3
drotherm <danielle_rothermel@brown.edu>
2014-04-27 21:39:03 -0400

    cmake is working properly now and so are the math fxns

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -55,5 +55,5 @@ SET(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 SET(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/main.dir/DependInfo.cmake"
+  "CMakeFiles/water_main.dir/DependInfo.cmake"
   )
